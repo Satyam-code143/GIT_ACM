1e05573d654954b80c20d96cf239b4301c871987
Satyam-code143 <Satyam-code143@github.com>
2021-04-17 15:44:53 +0530

    gh-pages and host

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Components/Home/Home";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <Router basename="/ACM-GIT">
+      <Route path="/" exact component={Home} />
+    </Router>
+  );
 }
 
 export default App;
